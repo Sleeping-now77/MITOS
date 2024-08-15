0c69196a656257c0b8f82e953f4cb1a7a4e331ee
YRY <1123166643@qq.com>
2024-08-15 16:53:45 +0800

    trace-okok!

--- Makefile
+++ Makefile
@@ -188,11 +188,8 @@ UPROGS=\
 	$U/_grind\
 	$U/_wc\
 	$U/_zombie\
-<<<<<<< Updated upstream
-
-=======
 	$U/_trace\
->>>>>>> Stashed changes
+
 
 
 
--- user/trace.c
+++ user/trace.c
@@ -1,5 +1,3 @@
-<<<<<<< Updated upstream
-=======
 // You have to modify the xv6 kernel to print out a line when each system call is about to return, 
 // if the system call's number is set in the mask.
 
@@ -7,35 +5,12 @@
 
 //The trace system call should enable tracing for the process that calls it and any children that it subsequently forks,
 
->>>>>>> Stashed changes
+
 #include "kernel/param.h"
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user/user.h"
 
-<<<<<<< Updated upstream
-int
-main(int argc, char *argv[])
-{
-  int i;
-  char *nargv[MAXARG];
-
-  if(argc < 3 || (argv[1][0] < '0' || argv[1][0] > '9')){
-    fprintf(2, "Usage: %s mask command\n", argv[0]);
-    exit(1);
-  }
-
-  if (trace(atoi(argv[1])) < 0) {
-    fprintf(2, "%s: trace failed\n", argv[0]);
-    exit(1);
-  }
-  
-  for(i = 2; i < argc && i < MAXARG; i++){
-    nargv[i-2] = argv[i];
-  }
-  exec(nargv[0], nargv);
-  exit(0);
-=======
 // trace 32 grep hello README
 
 int
@@ -59,5 +34,5 @@ main(int argc, char *argv[])
     }
     exec(nargv[0], nargv);
     exit(0);
->>>>>>> Stashed changes
+
 }
